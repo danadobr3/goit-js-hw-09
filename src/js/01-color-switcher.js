30ce8fae5f04219e72c414b7b9b9d73747500e56
danadobr3 <134941956+danadobr3@users.noreply.github.com>
2023-10-13 22:56:39 +0200

    hw1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const dataStart = document.querySelector('[data-start]');
+const dataStop = document.querySelector('[data-stop]');
+const bodyLink = document.querySelector('body');
 
+let timerId = null;
+
+dataStart.addEventListener('click', onStart);
+dataStop.addEventListener('click', onStop);
+
+function onStart() {
+  timerId = setInterval(getBgColor, 1000);
+
+  dataStart.setAttribute('disabled', 'true');
+}
+
+function onStop() {
+  clearInterval(timerId);
+
+  dataStart.removeAttribute('disabled');
+}
+
+function getBgColor() {
+  bodyLink.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
